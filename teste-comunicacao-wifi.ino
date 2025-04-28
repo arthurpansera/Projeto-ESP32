1e71615a7157f97f90c3517ab19c6831c8d98fa0
arthurpansera <163167633+arthurpansera@users.noreply.github.com>
2025-04-28 20:49:58 -0300

    Arrumando Código
    Arrumando o código teste de comunicação WiFi

--- teste-comunicacao-wifi.ino
+++ teste-comunicacao-wifi.ino
@@ -2,12 +2,12 @@
 #include <FirebaseESP32.h>
 
 // Credenciais Wi-Fi
-const char* ssid = "Seu_SSID";
-const char* password = "Sua_Senha";
+const char* ssid = "Visitantes";
+const char* password = "";
 
 // Configurações do Firebase
-#define FIREBASE_HOST "seu-projeto.firebaseio.com"  // URL do seu Firebase
-#define FIREBASE_AUTH "seu-token-de-autenticacao"
+#define FIREBASE_HOST "monitorado-consumo-agua.firebaseio.com"
+#define FIREBASE_AUTH ""
 
 // Inicializa o Firebase
 FirebaseData firebaseData;
@@ -29,7 +29,7 @@ void setup() {
 
 void loop() {
   // Envia dados simulados para o Firebase
-  float consumoAgua = 10.5; // Exemplo de valor para consumo de água
+  float consumoAgua = 10.5;
 
   if (Firebase.setFloat(firebaseData, "/consumoAgua", consumoAgua)) {
     Serial.println("Dados enviados para o Firebase!");
@@ -38,5 +38,5 @@ void loop() {
     Serial.println(firebaseData.errorReason());
   }
 
-  delay(5000);  // Envia a cada 5 segundos
+  delay(5000);
 }
