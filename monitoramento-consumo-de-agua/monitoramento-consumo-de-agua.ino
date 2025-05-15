404639e68ca8610695ee57ac8973c16fca0b39a9
Stefany Carlos <162990253+StefanyCarlos23@users.noreply.github.com>
2025-05-15 20:33:35 -0300

    Projeto ESP32
    Conexão com Wi-Fi e FireBase realizadas, sensor de consumo de água  funcionado e enviando todos os dados para o FireBase em tempo real.

---
+++ monitoramento-consumo-de-agua/monitoramento-consumo-de-agua.ino
@@ -0,0 +1,120 @@
+#include <WiFi.h>
+#include <Firebase_ESP_Client.h>
+#include <addons/TokenHelper.h>
+#include "time.h"
+
+// ----------- CONFIGURAÇÕES DE CONEXÃO WI-FI E FIREBASE -----------
+#define WIFI_SSID "TIM ULTRAFIBRA_0D10"
+#define WIFI_PASSWORD "QFjp@975tG"
+
+#define API_KEY "AIzaSyAlSmPYQo6f2fzR5SVXMNWQd02579OWxqY"
+#define DATABASE_URL "https://blueflow-7e0d7-default-rtdb.firebaseio.com/"
+
+// ----------- CONFIGURAÇÕES DO NTP -----------
+const char* ntpServer = "pool.ntp.org";
+const long gmtOffset_sec = -10800; //
+const int daylightOffset_sec = 0;
+
+// ----------- VARIÁVEIS SENSOR -----------
+volatile int pulsos = 0;
+float vazao_Lmin = 0.0;
+unsigned long tempoUltimaLeitura = 0;
+const unsigned long intervaloLeitura = 1000;
+
+#define PINO_SENSOR 18
+const float fator_calibracao = 7.5;
+
+// ----------- OBJETOS DO FIREBASE -----------
+FirebaseData fbdo;
+FirebaseAuth auth;
+FirebaseConfig config;
+
+bool signupOK = false;
+
+void IRAM_ATTR contaPulso() {
+  pulsos++;
+}
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+
+  Serial.println("Inicializando sensor...");
+  pinMode(PINO_SENSOR, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(PINO_SENSOR), contaPulso, FALLING);
+
+  // Parte do wifi
+  Serial.println("Conectando ao Wi-Fi...");
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  unsigned long startWiFi = millis();
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+    if (millis() - startWiFi > 15000) {
+      Serial.println("\nFalha ao conectar Wi-Fi. Reiniciando...");
+      ESP.restart();
+    }
+  }
+  Serial.println("\n✅ Wi-Fi conectado!");
+  Serial.print("IP: ");
+  Serial.println(WiFi.localIP());
+
+  // Parte do NPT (horário)
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+  Serial.print("Sincronizando horário");
+  struct tm timeinfo;
+  while (!getLocalTime(&timeinfo)) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println("\nHorário sincronizado:");
+  Serial.println(&timeinfo, "%Y-%m-%d %H:%M:%S");
+
+  // Parte do FireBase
+  config.api_key = API_KEY;
+  config.database_url = DATABASE_URL;
+  auth.user.email = "";
+  auth.user.password = "";
+  config.token_status_callback = tokenStatusCallback;
+
+  Serial.println("Realizando signUp anônimo...");
+  if (Firebase.signUp(&config, &auth, "", "")) {
+    Serial.println("SignUp realizado com sucesso!");
+    signupOK = true;
+  } else {
+    Serial.printf("Erro no signUp: %s\n", config.signer.signupError.message.c_str());
+  }
+
+  Firebase.begin(&config, &auth);
+  Firebase.reconnectWiFi(true);
+}
+
+void loop() {
+  if (signupOK && Firebase.ready()) {
+    if (millis() - tempoUltimaLeitura >= intervaloLeitura) {
+      tempoUltimaLeitura = millis();
+
+      int pulsosPorSegundo = pulsos;
+      pulsos = 0;
+
+      vazao_Lmin = (float)pulsosPorSegundo / fator_calibracao;
+
+      Serial.print("Pulsos por segundo: ");
+      Serial.print(pulsosPorSegundo);
+      Serial.print(" | Vazão: ");
+      Serial.print(vazao_Lmin);
+      Serial.println(" L/min");
+
+      if (Firebase.RTDB.setFloat(&fbdo, "/leituras/vazao_Lmin", vazao_Lmin)) {
+        Serial.println("✅ Dado enviado com sucesso!");
+      } else {
+        Serial.print("❌ Erro ao enviar dado: ");
+        Serial.println(fbdo.errorReason());
+      }
+    }
+  } else {
+    if (!signupOK) Serial.println("⚠️ SignUp não realizado.");
+    if (!Firebase.ready()) Serial.println("⚠️ Firebase não está pronto.");
+    delay(2000);
+  }
+}
